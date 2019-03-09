2b971d78deb41bf082a05979cc558fbe02e8de34
dharmendra chouhan <dharmendra.falna@gmail.com>
2019-03-09 18:16:54 +0530

    pagination

--- blog/models.py
+++ blog/models.py
@@ -19,25 +19,24 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,unique_for_date='publish')
     author = models.ForeignKey(
-        User,on_delete=models.CASCADE,related_name='blog_posts')
+    User,on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10,choices=STATUS_CHOICES,default='draft')
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager
 
 
-class Meta:
-    ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
-def get_absolute_url(self):
-    return reverse('blog:post_detail',
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
                     args=[self.publish.year,
                         self.publish.month,
                         self.publish.day,
